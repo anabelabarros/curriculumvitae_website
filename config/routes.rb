92c0023afa2f50807466330cf15477cb2aee0afc
Anabela B <anabelasb89@gmail.com>
2024-05-31 18:49:46 +0100

    Added views and form handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/summary'
-  get 'pages/experience'
-  get 'pages/skills'
-  get 'pages/projects'
-  get 'pages/education'
-  get 'pages/contact'
+  # get 'pages/home'
+  # get 'pages/summary'
+  # get 'pages/experience'
+  # get 'pages/skills'
+  # get 'pages/projects'
+  # get 'pages/education'
+  # get 'pages/contact'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  root 'pages#home'
+  get 'summary', to: 'pages#summary'
+  get 'experience', to: 'pages#experience'
+  get 'skills', to: 'pages#skills'
+  get 'projects', to: 'pages#projects'
+  get 'education', to: 'pages#education'
+  get 'contact', to: 'pages#contact'
 
   # Defines the root path route ("/")
   # root "posts#index"
